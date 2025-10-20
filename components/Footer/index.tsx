4afb161dde7eea78f2a225a219527a33b8eda17b
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-10-20 12:06:54 +0300

    use self-closing tags for empty divs

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
 		<footer className='bg-white border-t border-gray-200 mt-8 lg:mt-12 py-6 sm:py-8 lg:py-10'>
 			<div className='container-1920 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center'>
 				<p className='text-sm sm:text-base lg:text-lg text-1920-lg text-gray-600'>
-					&copy; 2024 <span className='font-semibold text-blue-600'>FakeStore</span>. All rights reserved.
+					&copy; 2024 <span className='font-semibold text-blue-600'>FakeStore</span>
 				</p>
 			</div>
 		</footer>
--- components/ProductList/index.tsx
+++ components/ProductList/index.tsx
@@ -23,7 +23,7 @@ export default function ProductList({ products }: Props) {
 	if (isLoading)
 		return (
 			<div className='flex justify-center items-center py-12 lg:py-16'>
-				<div className='animate-spin rounded-full h-8 w-8 sm:h-10 sm:w-10 lg:h-12 lg:w-12 border-b-2 border-blue-600'></div>
+				<div className='animate-spin rounded-full h-8 w-8 sm:h-10 sm:w-10 lg:h-12 lg:w-12 border-b-2 border-blue-600' />
 			</div>
 		)
 
