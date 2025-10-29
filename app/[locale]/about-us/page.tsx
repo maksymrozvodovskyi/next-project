ea7bb52e3328ad1037dc75189b66ae8459cb7eff
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-10-29 12:41:14 +0200

    add animation to block about us

--- app/[locale]/about-us/page.tsx
+++ app/[locale]/about-us/page.tsx
@@ -1,6 +1,6 @@
 export default function AboutUs() {
 	return (
-		<main className='min-h-screen flex flex-col items-center justify-center p-8 space-y-6'>
+		<main className='min-h-screen flex flex-col items-center justify-center p-8 space-y-6 animate-page-fade'>
 			<h1 className='text-3xl font-bold text-gray-800'>About Us</h1>
 			<p className='text-gray-600 text-center max-w-xl'>
 				Welcome to my project! Here’s a short video that tells a bit more about us.
